c7deae10038e22317910434b9c27668126f0f673
Ciaran Maloy <ciaranmaloy@Ciarans-MacBook-Air.local>
2023-06-20 19:04:11 +0100

    Added database and about to configure properly

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>login</p>'
+    return render_template('login.html', text="testing", user="Tim")
 
 @auth.route('/logout')
 def logout():
-    return '<p>logout</p>'
+    return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>sign-up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstName) < 2:
+            flash('firstname must be greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be longer than 6 characters', category='error')
+        else: 
+            # add usere to database
+            flash('Account created', category='success')
+            pass
+
+    return render_template('sign_up.html')
